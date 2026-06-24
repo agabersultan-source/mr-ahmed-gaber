3711721d4eb9b53937dbbeaeeafbc24cb5c71ead
agabersultan-source <agabersultan@gmail.com>
2026-06-24 07:18:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'al-mouassis-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
